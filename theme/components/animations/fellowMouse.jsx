30fdf9914266f413c1538803e81eaa561dfbd0b7
HainingCui <haining.cui@ntddigital.com>
2025-01-07 17:51:01 +0900

    t

---
+++ theme/components/animations/fellowMouse.jsx
@@ -0,0 +1,28 @@
+"use client"
+import React from "react";
+import { motion } from "framer-motion";
+import useMoveMentAnimation from "@/hooks/useMoveMentAnimation";
+
+const FollowMouse = ({ children }) => {
+    const [mousePositionX, mousePositionY] = useMoveMentAnimation();
+
+    return (
+        <motion.div
+            style={{
+               position: "relative", // Set absolute positioning to make the element move with the mouse
+                top: mousePositionY,  // Update the element's vertical position based on mouse Y position
+                left: mousePositionX, // Update the element's horizontal position based on mouse X position
+                pointerEvents: "none",  // Disable pointer events to avoid interference with mouse interactions
+            }}
+            transition={{
+                type: "spring",  // Use spring-type transition for smooth animation
+                stiffness: 100,  // Set the stiffness of the spring (controls how bouncy the movement is)
+                damping: 25,  // Set the damping of the spring (controls the smoothness of the movement)
+            }}
+        >
+            {children}  {/* Render child elements inside the FollowMouse container */}
+        </motion.div>
+    );
+};
+
+export default FollowMouse;
---
+++ theme/components/animations/slideLeft.jsx
@@ -0,0 +1,32 @@
+"use client"
+import { motion } from "framer-motion"
+
+const SlideLeft = ({ children }) => {
+    const slideLeftVariants = {
+        offscreen: {
+            x: -80,
+            opacity:0
+        },
+        onscreen: {
+            x: 0,
+            opacity:1,
+            transition: {
+                // type: "spring",
+                // bounce: 0.4,
+                duration: 0.8
+            }
+        }
+    };
+    return (
+        <motion.div
+            variants={slideLeftVariants}
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.1 }}
+        >
+            {children}
+        </motion.div>
+    )
+}
+
+export default SlideLeft

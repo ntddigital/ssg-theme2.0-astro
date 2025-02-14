3e1ec4ada879ae58935e4d3d8da5d38ba06cf45c
HainingCui <haining.cui@ntddigital.com>
2025-02-15 08:53:28 +0900

    t

--- theme/types.d.ts
+++ theme/types.d.ts
@@ -228,7 +228,7 @@ export interface Stats extends Omit<Headline, 'classes'>, Widget {
 export interface Pricing extends Omit<Headline, 'classes'>, Widget {
   prices?: Array<Price>;
   columns?: number
-  tableItems: any
+  tableItems?: any[]
 }
 
 export interface Testimonials extends Omit<Headline, 'classes'>, Widget {
